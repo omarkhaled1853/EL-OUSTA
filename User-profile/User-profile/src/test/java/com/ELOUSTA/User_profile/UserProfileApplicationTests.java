6035af6b38249387c1b3e744c8d74a5eb2896756
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-11-28 02:47:01 +0200

    EO-57 Intialize spring boot project

---
+++ User-profile/User-profile/src/test/java/com/ELOUSTA/User_profile/UserProfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ELOUSTA.User_profile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
