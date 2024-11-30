f2770c4169ee3eae6cc7a9a26cd064b497ca3064
omar khaled ibrahim <112958415+omarkhaled1853@users.noreply.github.com>
2024-11-30 17:00:54 +0200

    Eo 57 profile backend (#6)
    * EO-57 Intialize spring boot project
    
    * EO-57 Adding User and Client entities
    
    * EO-57 Adding Client repository
    
    * EO-57 Edit User and Client entities
    
    * EO-57 Adding more attributes to User entity
    
    * EO-57 Adding Technician entity with the related entities
    
    * EO-57 Adding configuration of database connection
    
    * EO-57 Adding Repositories
    
    * EO-57 Renamming project
    
    ---------
    
    Co-authored-by: Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>

---
+++ Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/entity/Client.java
@@ -0,0 +1,12 @@
+package com.ELOUSTA.Profile_backend.entity;
+
+import jakarta.persistence.Entity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@EqualsAndHashCode(callSuper = true)
+@Entity
+@Data
+@SuperBuilder
+public class Client extends User{
+}
---
+++ Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/entity/User.java
@@ -0,0 +1,31 @@
+package com.ELOUSTA.Profile_backend.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+import java.util.Date;
+
+@MappedSuperclass
+@Data
+@SuperBuilder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private Date dob;
+    private String userName;
+    private String phoneNumber;
+    private String email;
+    private String password;
+    private Date signUpDate;
+    private String governorate;
+    private String district;
+    private String roles;
+    private String profilePicture;
+}
