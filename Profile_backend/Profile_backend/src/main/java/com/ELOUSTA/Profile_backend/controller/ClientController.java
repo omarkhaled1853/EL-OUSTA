0c59494986828aec957141fd9749f2deb7db2be2
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-03 23:37:01 +0200

    EO-57 Adding end point to get profile photo

--- Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/controller/ClientController.java
+++ Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/controller/ClientController.java
@@ -4,9 +4,11 @@ import com.ELOUSTA.Profile_backend.dto.ClientDTO;
 import com.ELOUSTA.Profile_backend.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @RestController
@@ -25,5 +27,13 @@ public class ClientController {
         Optional<ClientDTO> clientDTO = clientService.getClient(id);
         return clientDTO.map(client -> new ResponseEntity<ClientDTO>(client, HttpStatus.OK))
                 .orElse(new ResponseEntity<ClientDTO>(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping({"path"})
+    public ResponseEntity<byte[]> getProfilePhoto(@PathVariable String path) throws IOException {
+        byte[] profilePhoto = clientService.getProfilePhoto(path);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(profilePhoto);
     }
 }
