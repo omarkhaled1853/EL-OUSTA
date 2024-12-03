fddaf839c0c83a9d1ce7476da1923f3c3d3a00f8
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-03 22:00:07 +0200

    EO-57 Adding query for getting technician with domain and and portfolio

--- Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/repository/TechnicianRepository.java
+++ Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/repository/TechnicianRepository.java
@@ -2,6 +2,14 @@ package com.ELOUSTA.Profile_backend.repository;
 
 import com.ELOUSTA.Profile_backend.entity.TechnicianEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface TechnicianRepository extends JpaRepository<TechnicianEntity, Integer> {
+    @Query("SELECT t " +
+            "FROM TechnicianEntity t " +
+            "JOIN FETCH t.domainEntity d " +
+            "LEFT JOIN FETCH t.portfolioEntities p " +
+            "WHERE t.id = :technicianId")
+    TechnicianEntity findTechnicianWithDomainAndPortfolio(@Param("technicianId") Integer technicianId);
 }
--- Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/service/impl/TechnicianServiceImpl.java
+++ Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/service/impl/TechnicianServiceImpl.java
@@ -1,10 +1,7 @@
 package com.ELOUSTA.Profile_backend.service.impl;
 
 import com.ELOUSTA.Profile_backend.dto.TechnicianDTO;
-import com.ELOUSTA.Profile_backend.entity.TechnicianEntity;
-import com.ELOUSTA.Profile_backend.repository.TechnicianRepository;
 import com.ELOUSTA.Profile_backend.service.TechnicianService;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.Optional;
 
