0af6a1c357f159e42dcbcbdba4a3b4d7fac1e09f
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-05 21:45:12 +0200

    EO-71 Creating new project

---
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/EloustaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ELOUSTA.ELOUSTA.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EloustaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EloustaBackendApplication.class, args);
+	}
+
+}
---
+++ ELOUSTA backend/ELOUSTA-backend/src/test/java/com/ELOUSTA/ELOUSTA/backend/EloustaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ELOUSTA.ELOUSTA.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EloustaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
