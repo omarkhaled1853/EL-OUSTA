5ef9f8ce2de63afb5ceb91af00bea2cb1f684866
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-06 16:51:38 +0200

    EO-71 Adding home technician dto

---
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/dto/HomeTechnicianDTO.java
@@ -0,0 +1,12 @@
+package com.ELOUSTA.ELOUSTA.backend.dto;
+
+
+public class HomeTechnicianDTO {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private DomainDTO domainDTO;
+    private int experience;
+    private Double rate;
+}
