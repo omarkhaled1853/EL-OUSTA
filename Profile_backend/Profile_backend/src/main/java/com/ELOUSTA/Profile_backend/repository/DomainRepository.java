94d2b1f71584ec4eb60fb41331ab66241c18b63e
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-07 21:29:14 +0200

    EO-71 Delete unneeded folders

--- Profile_backend/Profile_backend/src/main/java/com/ELOUSTA/Profile_backend/repository/DomainRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ELOUSTA.Profile_backend.repository;
-
-import com.ELOUSTA.Profile_backend.entity.DomainEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DomainRepository extends JpaRepository<DomainEntity, Integer> {
-}
