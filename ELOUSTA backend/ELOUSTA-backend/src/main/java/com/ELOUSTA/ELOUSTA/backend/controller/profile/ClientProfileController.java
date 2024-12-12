c6e66f42418e38895b6477164d4aab1132debc8c
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-13 00:14:49 +0200

    EO-80 Update client profile controller

--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/profile/ClientProfileController.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/profile/ClientProfileController.java
@@ -13,30 +13,30 @@ import java.util.Optional;
 @CrossOrigin
 @RequestMapping("/client")
 public class ClientProfileController {
-    private final ClientProfileService clientService;
+    private final ClientProfileService clientProfileService;
 
     @Autowired
-    public ClientProfileController(ClientProfileService clientService) {
-        this.clientService = clientService;
+    public ClientProfileController(ClientProfileService clientProfileService) {
+        this.clientProfileService = clientProfileService;
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<ClientProfileProfileDTO> getClient(@PathVariable Integer id) {
-        Optional<ClientProfileProfileDTO> clientDTO = clientService.getClient(id);
+        Optional<ClientProfileProfileDTO> clientDTO = clientProfileService.getClient(id);
         return clientDTO.map(client -> new ResponseEntity<ClientProfileProfileDTO>(client, HttpStatus.OK))
                 .orElse(new ResponseEntity<ClientProfileProfileDTO>(HttpStatus.NOT_FOUND));
     }
 
     @DeleteMapping("/delete/profile_picture/{id}")
     public ResponseEntity<?> removeClientProfilePicture(@PathVariable Integer id) {
-        clientService.removeClientProfilePhoto(id);
+        clientProfileService.removeClientProfilePhoto(id);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @PatchMapping("/patch/password/{id}")
     public ResponseEntity<?> resetClientPassword(@PathVariable Integer id,
                                                  @RequestBody String newPassword) {
-        clientService.resetClientPassword(id, newPassword);
+        clientProfileService.resetClientPassword(id, newPassword);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
