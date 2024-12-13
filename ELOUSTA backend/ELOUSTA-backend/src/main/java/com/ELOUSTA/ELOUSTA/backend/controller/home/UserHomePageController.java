5a543f2997a096150d83cd529dc9aaeb424809d3
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-13 23:22:06 +0200

    Update endpoints of home and profile

--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/home/UserHomePageController.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/home/UserHomePageController.java
@@ -7,16 +7,14 @@ import com.ELOUSTA.ELOUSTA.backend.service.home.SearchTechnicianService;
 import com.ELOUSTA.ELOUSTA.backend.service.home.SortTechnicianService;
 import com.ELOUSTA.ELOUSTA.backend.model.HomePayload;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/client/home")
 public class UserHomePageController {
     private final FilterTechnicianService filterService;
     private final SearchTechnicianService searchService;
--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/profile/ClientProfileController.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/profile/ClientProfileController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/client")
+@RequestMapping("/client/profile")
 public class ClientProfileController {
     private final ClientProfileService clientService;
 
