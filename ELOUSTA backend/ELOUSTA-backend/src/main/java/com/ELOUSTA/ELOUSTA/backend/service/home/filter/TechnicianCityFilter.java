6ab3249526cd9080a0b71157d104007c1ddf8c1f
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-12-14 19:02:08 +0200

    modify a little bit in filter algorithms

--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/service/home/filter/TechnicianCityFilter.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/service/home/filter/TechnicianCityFilter.java
@@ -16,7 +16,7 @@ public class TechnicianCityFilter implements ITechFilter {
             return new ArrayList<>();
         ArrayList<Technician>Answer=new ArrayList<>();
         for (Technician tech :technicians) {
-            if(tech.getCity().equals(query))
+            if(tech.getCity().contains(query))
                 Answer.add(tech);
         }
         return Answer;
