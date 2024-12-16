e76116b82b294d56ab336f67b05b774a271a6b42
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-12-16 11:22:28 +0200

    basic requests controller setup

--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/TechnicianRequests/TechnicianRequestsController.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/TechnicianRequests/TechnicianRequestsController.java
@@ -1,4 +1,38 @@
 package com.ELOUSTA.ELOUSTA.backend.controller.TechnicianRequests;
 
+import com.ELOUSTA.ELOUSTA.backend.service.technicianRequests.filterRequestsService;
+import com.ELOUSTA.ELOUSTA.backend.service.technicianRequests.searchRequestsService;
+import com.ELOUSTA.ELOUSTA.backend.service.technicianRequests.sortRequestsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/tech/requests")
 public class TechnicianRequestsController {
+
+    private final filterRequestsService filterService;
+
+    private final searchRequestsService searchService;
+    private final sortRequestsService sortService;
+
+    @Autowired
+    public TechnicianRequestsController(filterRequestsService filterService, searchRequestsService searchService, sortRequestsService sortService) {
+        this.filterService = filterService;
+        this.searchService = searchService;
+        this.sortService = sortService;
+    }
+
+    //TODO : Getters
+
+
+
+    //TODO: filters
+
+
+    //TODO: searching
+
+
+    //TODO: sorting
+
 }
