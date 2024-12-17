c126106f856bf2b8569548518d384d3b1d1bc363
Omer Khaled <Omer Khaled@DESKTOP-IL45J4J>
2024-12-17 23:28:09 +0200

    EO-100 Adding search,sort and filter queries in request repository

--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/repository/RequestRepository.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/repository/RequestRepository.java
@@ -29,6 +29,18 @@ public interface RequestRepository extends JpaRepository<RequestEntity,Integer> 
 //    @Query(value = "SELECT * FROM REQUEST WHERE ID= :id AND TECHID= :techId AND USERID = :clientId")
 
     @Query(value = "SELECT * FROM REQUEST WHERE USERID = :userId AND STATE= :state", nativeQuery = true)
-    List<RequestEntity> getClientRequestsByState(@Param("userId")int techId,@Param("state")String state);
+    List<RequestEntity> getClientRequestsByState(@Param("userId")int userId,@Param("state")String state);
+
+    @Query(value = "SELECT * FROM REQUEST WHERE USERID = :techId AND STATE= :state ORDER BY START_DATE", nativeQuery = true)
+    List<RequestEntity> sortClientRequestsByStartDate(@Param("userId") int userId,@Param("state")String state);
+
+    @Query(value="SELECT * FROM REQUEST WHERE USERID = :techId AND STATE= :state ORDER BY END_DATE", nativeQuery = true)
+    List<RequestEntity> sortClientRequestsByEndDate(@Param("userId") int userId,@Param("state")String state);
+
+    @Query(value = "SELECT * FROM REQUEST WHERE USERID = :techId AND STATE = :state AND LOCATION LIKE CONCAT('%', :filterTerm, '%')", nativeQuery = true)
+    List<RequestEntity> filterClientRequestsByLocation(@Param("userId") int userId, @Param("state") String state, @Param("filterTerm") String filterTerm);
+
+    @Query(value = "SELECT * FROM REQUEST WHERE USERID = :techId AND STATE = :state AND DESCRIPTION LIKE CONCAT('%', :searchTerm, '%')", nativeQuery = true)
+    List<RequestEntity> searchClientRequestsByDescription(@Param("userId") int userId, @Param("state") String state, @Param("searchTerm") String searchTerm);
 
 }
