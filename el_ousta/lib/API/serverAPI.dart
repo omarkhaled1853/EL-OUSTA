0dc04d711da69df387e466447fc755d5a5bee880
MohamedAhmed <mohammedahmmed4920@gmail.com>
2024-12-17 12:57:53 +0200

    add notification in front

--- el_ousta/lib/API/serverAPI.dart
+++ el_ousta/lib/API/serverAPI.dart
@@ -1,4 +1,5 @@
 class ServerAPI {
-  static const String baseURL = "http://192.168.1.6:8080";
+  // static const String baseURL = "http://192.168.1.6:8080";
+  static const String baseURL = "http://10.0.2.2:8080";
   // static const String baseURLmail = "http://192.168.194.21:8083";
 }
---
+++ el_ousta/lib/widgets/appBarWithNotification.dart
@@ -0,0 +1,211 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:stomp_dart_client/stomp.dart';
+import 'package:stomp_dart_client/stomp_config.dart';
+import 'package:el_ousta/models/Notification.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Notification App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: NotificationScreen(),
+//     );
+//   }
+// }
+
+class NotificationScreen extends StatefulWidget {
+  @override
+  _NotificationScreenState createState() => _NotificationScreenState();
+}
+
+class _NotificationScreenState extends State<NotificationScreen> {
+  late StompClient stompClient;
+  bool isConnected = false;
+  int notificationCount = 0;
+  final int userId = 1; // Change to the actual user ID
+  final String token = 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqb2huZG9lIiwiaWF0IjoxNzM0NDI3MTI1LCJleHAiOjE3MzQ1MTM1MjV9.ys0dqqEwTETu4igdTWLvAds9-AnGEdPzl8Lev0vGt50'; // Replace with your Bearer token
+
+  List<NotificationObject> notifications = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchNotifications(); // Fetch notifications from backend on initial load
+    initializeWebSocket();
+  }
+
+  @override
+  void dispose() {
+    stompClient.deactivate();
+    super.dispose();
+  }
+
+  // Fetch notifications from backend
+  Future<void> fetchNotifications() async {
+    final url = 'http://10.0.2.2:8080/client/notifications/$userId';
+
+    final response = await http.get(
+      Uri.parse(url),
+      headers: {
+        'Authorization': 'Bearer $token', // Add Bearer token to Authorization header
+      },
+    );
+
+    if (response.statusCode == 200) {
+      List<dynamic> data = json.decode(response.body);
+      setState(() {
+        notifications = data.map((item) => NotificationObject.fromJson(item)).toList();
+      });
+    } else {
+      throw Exception('Failed to load notifications');
+    }
+  }
+
+  // Initialize WebSocket for real-time notifications
+  void initializeWebSocket() {
+    stompClient = StompClient(
+      config: StompConfig(
+        url: 'ws://10.0.2.2:8080/elousta-websocket', // Replace with your WebSocket URL
+        onConnect: (frame) {
+          setState(() {
+            isConnected = true;
+          });
+          print('Connected: $frame');
+
+          // Subscribe to the notifications topic for the specific user
+          stompClient.subscribe(
+            destination: '/subscribe/client/$userId',
+            callback: (frame) {
+              if (frame.body != null) {
+                setState(() {
+                  // Parse the incoming notification and add to the list
+                  notifications.add(NotificationObject.fromJson(json.decode(frame.body!)));
+                  notificationCount++;
+                });
+              }
+            },
+          );
+        },
+        onWebSocketError: (error) {
+          print('WebSocket error: $error');
+        },
+        onStompError: (frame) {
+          print('STOMP error: ${frame.headers['message']}');
+        },
+      ),
+    );
+
+    stompClient.activate();
+  }
+
+  // Reset the notification count when the user views notifications
+  void resetNotificationCount() {
+    setState(() {
+      notificationCount = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: Text('Notification App'),
+      actions: [
+        PopupMenuButton<String>(
+          icon: Stack(
+            children: [
+              Icon(Icons.notifications),
+              if (notificationCount > 0)
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(2),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      shape: BoxShape.circle,
+                    ),
+                    child: Text(
+                      '$notificationCount',
+                      style: TextStyle(color: Colors.white, fontSize: 12),
+                    ),
+                  ),
+                ),
+            ],
+          ),
+          onSelected: (String value) {
+            // Handle selection if necessary
+          },
+          onCanceled: () {
+            resetNotificationCount();
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              PopupMenuItem<String>(
+                child: Container(
+                  padding: EdgeInsets.all(10),
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 1,
+                        blurRadius: 4,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: notifications.isNotEmpty
+                      ? ListView.builder(
+                    itemCount: notifications.length,
+                    itemBuilder: (context, index) {
+                      final notification = notifications[index];
+                      return Container(
+                        margin: EdgeInsets.only(bottom: 10),
+                        decoration: BoxDecoration(
+                          color: Colors.lightBlue[50],
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(
+                            horizontal: 15,
+                            vertical: 10,
+                          ),
+                          title: Text(
+                            notification.message,
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                          subtitle: Text(
+                            '${notification.date}',
+                            style: TextStyle(fontSize: 12),
+                          ),
+                          tileColor: Colors.white,
+                        ),
+                      );
+                    },
+                  )
+                      : Center(child: Text('No notifications available.')),
+                ),
+              ),
+            ];
+          },
+        ),
+      ],
+      // body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
