acfb4b28b5aad1ba5d9dc8a13cc8a5b655882c87
محمد حسن <nhmdh7092@gmail.com>
2024-12-29 20:59:28 +0200

    version with no testing

--- ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/Admin/HomeController.java
+++ ELOUSTA backend/ELOUSTA-backend/src/main/java/com/ELOUSTA/ELOUSTA/backend/controller/Admin/HomeController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 @RestController
-@RequestMapping("/admin/home/")
+@RequestMapping("/admin/home")
 public class HomeController {
     @Autowired
     private RequestService requestService;
